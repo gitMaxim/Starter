e1db3be03287259eb2e46afa189f969d12de525c
gitMaxim <noreplygit@yandex.com>
2015-05-23 02:13:15 +0300

    Patterns folder created.

---
+++ Patterns/Strategy/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ModuleDuckPattern;
+
+// Pattern   #1 (p. 60)
+//
+// Name     "Strategy"
+//   
+// TL; DR   Incapsulates each family of algorithms, so we can modify
+//          them independently.
+namespace DuckPattern
+{
+    class MiniDuckSimulator
+    {
+        static void Main(string[] args)
+        {
+            Duck mallard = new MallardDuck();
+            mallard.PerformQuack();
+            mallard.PerformFly();
+
+            Duck model = new ModelDuck();
+            model.PerformFly();
+            model.setFlyBegavior(new FlyRocketPowered());
+            model.PerformFly();
+
+            Console.ReadLine();
+        }
+    }
+}
